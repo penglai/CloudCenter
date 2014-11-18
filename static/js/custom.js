4b379288cbd7be1178786637f4579b5edb620fa3
Reyrey <reyrey.zhang@profero.com>
2014-11-18 17:24:42 +0800

    add homepage

--- static/js/custom.js
+++ static/js/custom.js
@@ -38,6 +38,7 @@ $(function(){
  	}
  	$focusFt.append($btnNav);
  	$focusFt.find('a:first').addClass('on');
+ 	$focusFt.css('margin-left',-$focusFt.width()/2);
 
 
  	// window resize change width and left
@@ -104,20 +105,8 @@ $(function(){
  	})
  	
 
- 	// feature animate
- 	$('.feature .bd ul').each(function(){
- 		$(this).css('margin-top',-$(this).height()/2);
- 	})
- 	$(".feature .item").each(function(index) {
- 		$(this).hover(function(){
- 			$(this).find('.hd').hide();
- 			$(this).find('.bd').show();
- 		},function(){
- 			$(this).find('.bd').hide();
- 			$(this).find('.hd').show();
- 		})
-	});
 
+ 	
 	 // switch tab
 	function tab(controller,content){
 		controller.each(function(){
@@ -132,39 +121,4 @@ $(function(){
 	tab($('.news .tab li'),$('.news .tab_content ul'));
 	tab($('.services .tab li'),$('.services .tab_content .list'));
 	tab($('.park_overview .tab li'),$('.park_overview .tab_content .list'));
- 	
-
-	// 合作伙伴轮播
-	/*********箭头控制滚动方向、加速及鼠标拖动***************/
-
-	/*********箭头控制滚动方向、加速及鼠标拖动***************/
-
-	var MarqueeControl=new Marquee("Marquee");      //箭头控制滚动方向、加速及鼠标拖动实例
-
-	MarqueeControl.Direction="left";
-
-	MarqueeControl.Step=1;
-
-	MarqueeControl.Width=910;
-
-	MarqueeControl.Height=60;
-
-	MarqueeControl.Timer=10;
-
-	MarqueeControl.ScrollStep=1;                //若为-1则禁止鼠标控制实例
-
-	MarqueeControl.Start();
-
-	MarqueeControl.BakStep=MarqueeControl.Step;
-
-	xg("prev").onmouseover=function(){MarqueeControl.Direction=2};
-
-	xg("prev").onmouseout=xg("prev").onmouseup=function(){MarqueeControl.Step=MarqueeControl.BakStep};
-
-	xg("prev").onmousedown=xg("next").onmousedown=function(){MarqueeControl.Step=MarqueeControl.BakStep+3};
-
-	xg("next").onmouseover=function(){MarqueeControl.Direction=3};
-
-	xg("next").onmouseout=xg("next").onmouseup=function(){MarqueeControl.Step=MarqueeControl.BakStep};
-
  });
