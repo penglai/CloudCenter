5e07da3ecef1f63fddf483814fca2899785fe51a
Reyrey <reyrey.zhang@profero.com>
2014-11-28 18:10:26 +0800

    update zol page

--- static/js/common.js
+++ static/js/common.js
@@ -1,9 +1,33 @@
 $(function(){
+	// nav
+	var $controlBar = $(".top_bar .nav li"),
+		$controlContent = $('.top_bar .sub_nav ul');
+
+	$controlBar.hover(function(){
+		_index = $(this).index();
+		current = $controlBar.find('a.current').parent().index();
+		if(_index>0){
+			$controlContent.eq(_index-1).addClass('current').siblings().removeClass('current');
+		}else{
+			$controlContent.removeClass('current');
+		}
+	},function(){
+		$controlContent.removeClass('current');
+		if(current>0){
+			$controlContent.eq(current-1).addClass('current');
+		}
+	});
+	$controlContent.find('a').on('click',function(){
+		$(this).addClass('current').parent().siblings().find('a').removeClass('current');
+		tab($(this).parent().index()+1);
+	})
+
 	// tab switch
 	function tab(index){
 		if(index){
-			$('.sidebar .switch_tab li').eq(index).addClass('current').siblings().removeClass('current');
-			$('.switch_content .swicth_content_section').eq(index).fadeIn().siblings().hide();
+			$('.top_bar .sub_nav li').eq(index-1).find('a').addClass('current').parent().siblings().find('a').removeClass('current');
+			$('.sidebar .switch_tab li').eq(index-1).addClass('current').siblings().removeClass('current');
+			$('.switch_content .swicth_content_section').eq(index-1).fadeIn().siblings().hide();
 		}
 		$('.sidebar .switch_tab li').on('click',function(){
 			$index = $(this).index();
@@ -27,16 +51,12 @@ $(function(){
 			console.log(subNavIndex);
 			switch(subNavIndex)
 			{
-				case '0' :
-				subNavIndex = '0';
-				break;
 				case '1' :
 				subNavIndex = '1';
 				break;
-				case '2':
-				subNavIndex = 2;
+				case '2' :
+				subNavIndex = '2';
 				break;
-
 				case '3':
 				subNavIndex = 3;
 				break;
@@ -53,9 +73,6 @@ $(function(){
 				subNavIndex = 6;
 				break;
 
-				case '7':
-				subNavIndex = 7;
-				break;
 				default:
 				subNavIndex = null;
 				break;
